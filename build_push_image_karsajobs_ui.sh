bfb4dbe7fdfa2c2ffde1fddf7b087040d8218501
hamonangann <bornyto.hamonangan@ui.ac.id>
2024-03-02 11:05:15 +0700

    add docker build script

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,8 @@
+# Membuat Docker image dengan nama karsajobs-ui dan memiliki tag latest. Dockerfile di current dir.
+docker build -t ghcr.io/hamonangann/karsajobs-ui:latest .
+
+# Login ke Github Packages
+cat ../.gh-pass.txt | docker login ghcr.io --username hamonangann --password-stdin
+
+# Push image ke Github Packages
+docker push ghcr.io/hamonangann/karsajobs-ui:latest
